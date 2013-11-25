26f38a0061f2ea6b80f6a314b6feb9b97c1f11cd
Trevor Cooper <tcooper@sdsc.edu>
2013-11-25 14:28:53 -0800

    Updated Makefile and version.mk to use pull.mk

--- src/siesta/version.mk
+++ src/siesta/version.mk
@@ -1,3 +1,14 @@
-NAME    = siesta_$(ROLLCOMPILER)_$(ROLLMPI)_$(ROLLNETWORK)
-VERSION	= 3.1
-RELEASE	= 1
+NAME               = siesta_$(ROLLCOMPILER)_$(ROLLMPI)_$(ROLLNETWORK)
+VERSION            = 3.1
+RELEASE            = 1
+PKGROOT            = /opt/siesta
+
+SRC_SUBDIR         = siesta
+
+SOURCE_NAME        = siesta
+SOURCE_VERSION     = $(VERSION)
+SOURCE_SUFFIX      = tgz
+SOURCE_PKG         = $(SOURCE_NAME)-$(SOURCE_VERSION).$(SOURCE_SUFFIX)
+SOURCE_DIR         = $(SOURCE_PKG:%.$(SOURCE_SUFFIX)=%)
+
+TGZ_PKGS           = $(SOURCE_PKG)
