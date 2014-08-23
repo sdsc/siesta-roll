6b5fcf15528d16e192bb666e61ed2b405b007fc3
Jim Hayes <jhayes@sdsc.edu>
2014-08-23 15:45:33 -0700

    Clean up build process.  Support versions in ROLLCOMPILER value.

--- src/siesta/version.mk
+++ src/siesta/version.mk
@@ -1,13 +1,25 @@
-NAME               = siesta_$(ROLLCOMPILER)_$(ROLLMPI)_$(ROLLNETWORK)
-VERSION            = 3.2.pl.4
-RELEASE            = 0
-PKGROOT            = /opt/siesta
+ifndef ROLLCOMPILER
+  ROLLCOMPILER = gnu
+endif
+COMPILERNAME := $(firstword $(subst /, ,$(ROLLCOMPILER)))
+
+ifndef ROLLNETWORK
+  ROLLNETWORK = eth
+endif
+
+ifndef ROLLMPI
+  ROLLMPI = openmpi
+endif
+
+NAME               = siesta_$(COMPILERNAME)_$(ROLLMPI)_$(ROLLNETWORK)
+VERSION            = 3.2.4
+RELEASE            = 1
 RPM.EXTRAS         = AutoReq:No
 
 SRC_SUBDIR         = siesta
 
 SOURCE_NAME        = siesta
-SOURCE_VERSION     = 3.2-pl-4
+SOURCE_VERSION     = 3.2.4
 SOURCE_SUFFIX      = tgz
 SOURCE_PKG         = $(SOURCE_NAME)-$(SOURCE_VERSION).$(SOURCE_SUFFIX)
 SOURCE_DIR         = $(SOURCE_PKG:%.$(SOURCE_SUFFIX)=%)
