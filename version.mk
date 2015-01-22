96d2c29c8b9f808c5b5daf60081610614c5083de
Jerry Greenberg <jpg@tscc-0-62.sdsc.edu>
2015-01-21 22:29:41 -0800

    new version: 3.2.5

--- version.mk
+++ version.mk
@@ -1,5 +1,6 @@
-ROLLNAME = siesta
-RELEASE  = 0
-COLOR    = orange
+ROLLNAME        = siesta
+VERSION        :=$(shell bash version.sh -v)
+RELEASE        :=$(shell bash version.sh -h)
+COLOR           = orange
 
-REDHAT.ROOT = $(PWD)
+REDHAT.ROOT     = $(PWD)
