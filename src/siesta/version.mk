a66cfc2fa0c090816c4e12a32a915f6b0b0569b4
Jerry Greenberg <jpg@sdsc.edu>
2015-12-18 15:22:51 -0800

    APS-232: fixed version number again

--- src/siesta/version.mk
+++ src/siesta/version.mk
@@ -9,7 +9,7 @@ endif
 MPINAME := $(firstword $(subst /, ,$(ROLLMPI)))
 
 NAME           = sdsc-siesta
-VERSION        = 3.2-pl-6
+VERSION        = 3.2.6
 RELEASE        = 0
 PKGROOT        = /opt/siesta
 
@@ -17,7 +17,7 @@ SRC_SUBDIR     = siesta
 
 SOURCE_NAME    = siesta
 SOURCE_SUFFIX  = tgz
-SOURCE_VERSION = $(VERSION)
+SOURCE_VERSION = 3.2-pl-6
 SOURCE_PKG     = $(SOURCE_NAME)-$(SOURCE_VERSION).$(SOURCE_SUFFIX)
 SOURCE_DIR     = $(SOURCE_PKG:%.$(SOURCE_SUFFIX)=%)
 
