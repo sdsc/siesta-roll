ace1ef911663d175b0e9e04c8a215f8cca0587f0
Jerry Greenberg <jpg@sdsc.edu>
2019-01-30 15:41:10 -0800

    APS-988 change name of siesta tar file

--- src/siesta/version.mk
+++ src/siesta/version.mk
@@ -16,12 +16,12 @@ PKGROOT        = /opt/siesta
 SRC_SUBDIR     = siesta
 
 SOURCE_NAME    = siesta
-SOURCE_SUFFIX  = tgz
+SOURCE_SUFFIX  = tar.gz
 SOURCE_VERSION = $(VERSION)
 SOURCE_PKG     = $(SOURCE_NAME)-$(SOURCE_VERSION).$(SOURCE_SUFFIX)
 SOURCE_DIR     = $(SOURCE_PKG:%.$(SOURCE_SUFFIX)=%)
 
-TGZ_PKGS       = $(SOURCE_PKG)
+TAR_GZ_PKGS       = $(SOURCE_PKG)
 
 RPM.EXTRAS     = AutoReq:No\nAutoProv:No
 RPM.PREFIX     = $(PKGROOT)
